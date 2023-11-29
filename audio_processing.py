436c57055a853885745d65ebc92cc769ec16902e
Tim <Tim>
2023-11-29 12:31:29 -0600

    updates for transcription

--- audio_processing.py
+++ audio_processing.py
@@ -4,41 +4,43 @@ from datasets import load_dataset
 import librosa
 import soundfile as sf
 from moviepy.editor import VideoFileClip
+import json
 
-def stt(audio_path, chunk_duration=10):
+def stt(audio_path, t_output):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
     model_id = "openai/whisper-large-v3"
 
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, torch_dtype= torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
     model.to(device)
 
     processor = AutoProcessor.from_pretrained(model_id)
 
     pipe = pipeline(
-        "automatic-speech-recognition", 
-        model=model, 
-        tokenizer=processor.tokenizer, 
-        feature_extractor=processor.feature_extractor, 
-        max_new_tokens=128, 
-        chunk_length_s=30, 
-        batch_size=16, 
-        return_timestamps=True, 
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
         torch_dtype=torch_dtype,
-        device=device
-    )
+        device=device,
+    )    
 
-    dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
-    
-    result = pipe(audio_path)
-    print(result["text"])
+    result = pipe(audio_path, return_timestamps=True)
+    with open(t_output, "w") as f:
+        json.dump(result["chunks"], f)
 
-def extract_audio(video_path, i):
-    full_video_path = "./videos_" + str(i) + "/" + video_path
-    video = VideoFileClip(full_video_path)
+    return t_output
     
-    audio_path = "./videos_" + str(i) + "/audio.wav"
+
+def extract_audio(video_path, audio_path):
+    video = VideoFileClip(video_path)
     video.audio.write_audiofile(audio_path, codec='pcm_s16le', ffmpeg_params=['-ac', '1'])
     return audio_path
 
