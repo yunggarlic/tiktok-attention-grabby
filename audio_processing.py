c9baa95eab9048866bd127025a39cd96e17b48fc
Tim <ferrari.tim.v@gmail.com>
2024-03-16 19:35:44 -0500

    adds whitenoise and phase inversion

--- audio_processing.py
+++ audio_processing.py
@@ -3,8 +3,12 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from datasets import load_dataset
 import librosa
 import soundfile as sf
-from moviepy.editor import VideoFileClip, AudioFileClip
+from moviepy.editor import VideoFileClip, AudioFileClip, AudioClip
 import json
+import os
+from pydub import AudioSegment
+from pydub.generators import WhiteNoise
+import sys
 
 def stt(audio_path, t_output):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -13,7 +17,7 @@ def stt(audio_path, t_output):
     model_id = "openai/whisper-large-v3"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True, 
     )
     model.to(device)
 
@@ -32,19 +36,33 @@ def stt(audio_path, t_output):
         device=device,
     )    
 
-    result = pipe(audio_path, return_timestamps=True)
+    result = pipe(audio_path, return_timestamps=True, generate_kwargs={"language": "en"})
     with open(t_output, "w") as f:
         json.dump(result["chunks"], f)
 
     return t_output
     
 def extract_audio(asset_path, output_path):
+    print("Extracting audio from", asset_path)
     audio = AudioFileClip(asset_path)
-    audio_file_path = output_path + "/" + "audio.wav"
+    audio_file_path = os.path.join(output_path, "audio.wav")
     audio.write_audiofile(audio_file_path, codec='pcm_s16le', ffmpeg_params=['-ac', '1'])
     return audio_file_path
 
+def imperceptibly_edit_audio(audio_folders: list[str]):
+    [imperceptibly_edit_audio_file(AudioSegment.from_file(os.path.join(audio_path, 'audio.wav')), os.path.join(audio_path, 'audio.wav')) for audio_path in audio_folders]
+
+
+def imperceptibly_edit_audio_file(audio, audio_path):
+    duration = audio.duration_seconds
+    
+
+    white_noise = WhiteNoise().to_audio_segment(duration=duration).apply_gain(-70)
+    edited_audio = audio.overlay(white_noise, loop=True)    
+    edited_audio.invert_phase().export(audio_path, format="wav")
+
 def downsample_audio(audio_path):
+    print("Downsampling audio", audio_path)
     original_audio, original_sample_rate = librosa.load(audio_path, sr=None)
     downsampled_audio = librosa.resample(original_audio, orig_sr=original_sample_rate, target_sr=16000)
     sf.write(audio_path, downsampled_audio, 16000)
